57342fbedc5c692f9861e2d950689eb46c2cabc7
lehasm <lehasm@gmail.com>
2018-08-31 13:11:30 +0300

    Logging is enabled and configured in a single place - package init file Test package __all__ variable removed

--- regression_runner/__init__.py
+++ regression_runner/__init__.py
@@ -4,3 +4,10 @@ The package is used to run tests and analyse run output.
 
 from regression_input import *
 from action_templates import *
+
+import logging
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="[%(levelname)s] %(filename)s %(funcName)s():\n%(message)s"
+)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,3 @@
 """
 Test suite for package regression runner
 """
-__all__ = ["regression_input_tests", "RunObject_tests", "RunDescriptionObject_tests"]
