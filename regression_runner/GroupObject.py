3b737ff20e587036f033c60d767aa56bc32c2a91
lehasm <lehasm@gmail.com>
2018-08-31 17:49:48 +0300

    Group and Test class stubs

--- regression_runner/GroupObject.py
+++ regression_runner/GroupObject.py
@@ -1,2 +1,9 @@
-"""
-"""
+
+from RunDescriptionObject import RunDescriptionObject
+
+class GroupObject(RunDescriptionObject):
+    """ Class to manage groups  """
+
+    def __init__(self, name):
+        super(GroupObject, self).__init__(name)
+
--- regression_runner/TestObject.py
+++ regression_runner/TestObject.py
@@ -1,2 +1,7 @@
-"""
-"""
+from RunDescriptionObject import RunDescriptionObject
+
+class TestObject(RunDescriptionObject):
+    """ Class to manage groups  """
+
+    def __init__(self, name):
+        super(TestObject, self).__init__(name)
