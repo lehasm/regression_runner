c63d815e557b9dd52d625d17468f0c8c16d7e7ba
lehasm <lehasm@gmail.com>
2018-09-03 14:03:21 +0300

    Instead of import * explicit function names available to a user are imported.

--- regression_runner/__init__.py
+++ regression_runner/__init__.py
@@ -5,7 +5,7 @@ The package is used to run tests and analyse run output.
 # To call unit tests:
 # python -m unittest discover --start-directory tests --pattern *.py
 
-from regression_input import *
+from regression_input import Run, Test, Group
 from action_templates import *
 
 import logging
