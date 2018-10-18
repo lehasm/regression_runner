d400ef7d90d1bf78c1bb7c3da48b0bea17138d59
lehasm <lehasm@gmail.com>
2018-10-18 22:18:30 +0300

    Log file is opened in lazy manner when accessing corresponding property. (There seems to be a restriction when accessing file handlers opened in different processes)

--- regression_runner/ResultObject.py
+++ regression_runner/ResultObject.py
@@ -1,10 +1,25 @@
 
 class ResultObject():
-    """ Class to store test run results  """
+    """ Class to store test run results and to manage log file """
 
     def __init__(self, log_file_name = None):
         self.state = "waiting"
         self.timeout = False
         self.passed = False
-        self.log_file = open(log_file_name, "a") if log_file_name else None
+        self.log_file_name = log_file_name
+        self._log_file = None
+      
+    def __del__(self):
+        self.CloseLog()
+            
+    @property        
+    def log_file(self):
+        if self.log_file_name:
+            self._log_file = open(self.log_file_name, "a")
+        return self._log_file
+        
+    
+    def CloseLog(self):
+         if self._log_file:
+            self._log_file.close()
         
